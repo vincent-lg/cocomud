44c0b6ab898a27e33e7d9d5382805dcd7073f3f6
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2016-09-23 10:51:58 -0700

    Add the #play SharpScript function

--- src/sharp/__init__.py
+++ src/sharp/__init__.py
@@ -1,8 +1,10 @@
 ﻿"""Package containing the sharp script."""
 
 from sharp.function import Function
+from sharp.functions.play import Play
 from sharp.functions.say import Say
 
 FUNCTIONS = {
+    "play": Play,
     "say": Say,
 }
