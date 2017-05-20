1da0e093bd2a1fd70964dd0b80c0a554d46fd3e9
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2017-05-20 13:18:22 -0700

    Add tests for command stacking

---
+++ src/tests/client/test_stacking.py
@@ -0,0 +1,74 @@
+﻿# Copyright (c) 2016, LE GOFF Vincent
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+
+# * Neither the name of ytranslate nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from mock import MagicMock, call
+
+from models import MockClient
+from scripting.alias import Alias
+
+class TestCommandStacking(MockClient):
+
+    """Test of command stacking."""
+
+    def test_without(self):
+        """Test without any command stacking."""
+        self.client.write(u"say 1;say 2")
+        self.client.transport.write.assert_called_once_with(
+                "say 1;say 2\r\n")
+
+    def test_simple(self):
+        """Test simple command stacking with a ; sign."""
+        def get_setting(address):
+            """Private function to return a set of default settings."""
+            default = {
+                    "options.input.command_stacking": ";",
+                    "options.general.encoding": "utf-8",
+            }
+            return default[address]
+
+        self.client.factory.engine.settings.__getitem__ = MagicMock(
+                side_effect=get_setting)
+        self.client.write(u"say 1;say 2")
+        calls = [call("say 1\r\n"), call("say 2\r\n")]
+        self.client.transport.write.assert_has_calls(calls)
+
+    def test_special(self):
+        """Test command stacking with a special character."""
+        def get_setting(address):
+            """Private function to return a set of default settings."""
+            default = {
+                    "options.input.command_stacking": u"\x82",
+                    "options.general.encoding": "utf-8",
+            }
+            return default[address]
+
+        self.client.factory.engine.settings.__getitem__ = MagicMock(
+                side_effect=get_setting)
+        self.client.write(u"say 1\x82say 2")
+        calls = [call("say 1\r\n"), call("say 2\r\n")]
+        self.client.transport.write.assert_has_calls(calls)
