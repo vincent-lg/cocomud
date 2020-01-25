9b9865b7a53b65afa3a1f9e60713c536b36dff4e
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2020-01-25 16:10:36 +0100

    Complete #118: add the #randplay function

--- src/sharp/functions/randplay.py
+++ src/sharp/functions/randplay.py
@@ -28,12 +28,13 @@
 
 """Module containing the RandPlay function class."""
 
-import os
+from pathlib import Path
 from random import choice
 
 import wx
 from ytranslate import t
 
+from audio import audiolib
 from log import logger
 from sharp import Function
 
@@ -56,39 +57,65 @@ class RandPlay(Function):
         filenames = filenames.split(";")
         filename = choice(filenames)
         if self.engine.sounds:
-            log.debug("#randplay {}".format(repr(filename)))
+            log.debug(f"#randplay {filename!r}")
         else:
-            log.debug("#randplay-silent {}".format(repr(filename)))
+            log.debug(f"#randplay-silent {filename!r}")
             return
 
-        filename = self.find_abs_filename(filename)
-        if os.path.exists(filename):
-            log.debug("#randplay playing {}".format(repr(filename)))
+        files = self.find_files(filename)
+        if files:
+            filename = choice(files)
+            log.debug(f"#randplay playing {filename!r}")
+            audiolib.play(filename)
         else:
-            log.warning("#randplay cannot find the file at {}".format(
-                    repr(filename)))
+            log.warning(f"#randplay cannot find any sound")
 
-        sound = mixer.Sound(filename)
-        sound.play()
+    def find_files(self, filename):
+        """Return a list of existing files matching this filename."""
+        absolute = Path(filename)
+        if not absolute.is_absolute():
+            absolute = Path(self.world.path) / filename
 
-    def find_abs_filename(self, filename):
-        """Return the absolute path of the file.
+        # The last part in the file name is searched
+        parent = absolute.parent
+        match = absolute.parts[-1]
+        results = list(parent.rglob(match))
+        results = [path for path in results if path.is_file()]
+        return [str(path) for path in results]
 
-        The initial file name can be absolute, or relative to the
-        world's location.
+    def display(self, dialog, filenames=""):
+        """Display the function's argument."""
+        self.dialog = dialog
+        l_files = self.t("files", "Audio files to be played")
 
-        """
-        if os.path.isabs(filename):
-            pass
-        elif os.path.normpath(filename).split(os.path.sep)[0] == "worlds":
-            filename = os.path.abspath(filename)
-        else:
-            path = os.path.join(self.world.path, filename)
-            filename = os.path.abspath(path)
+        # Dialog
+        l_files = wx.StaticText(dialog, label=l_files)
+        t_files = wx.TextCtrl(dialog, value=filenames)
+        test = wx.Button(dialog, label=t("ui.button.test"))
+        dialog.files = t_files
+        dialog.top.Add(l_files)
+        dialog.top.Add(t_files)
+        dialog.top.Add(test)
 
-        return filename
+        # Event binding
+        test.Bind(wx.EVT_BUTTON, self.test_files)
 
-    def find_rel_filename(self, filename):
-        """Return the filename relative to the world's location."""
-        location = os.path.abspath(self.world.path)
-        return os.path.relpath(filename, location)
+    def complete(self, dialog):
+        """The user pressed 'ok' in the dialog."""
+        files = dialog.files.GetValue()
+        empty_path = self.t("empty_path",
+                "The path hasn't been set.  What file should I play?")
+        if not files:
+            wx.MessageBox(empty_path, t("ui.message.error"),
+                    wx.OK | wx.ICON_ERROR)
+            dialog.files.SetFocus()
+            return None
+
+        return (files, )
+
+    def test_files(self, e):
+        """Test the audio files."""
+        parent = self.dialog
+        names = parent.files.GetValue().split(";")
+        filename = choice(self.find_files(choice(names)))
+        audiolib.play(filename)
